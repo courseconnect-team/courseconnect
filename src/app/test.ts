efb36ec4b5a1b71b4d43fae835a8ac2cdde01ca7
norman-codes <78187320+norman-codes@users.noreply.github.com>
2023-05-22 15:52:53 -0400

    linter automation test

--- src/app/test.ts
+++ src/app/test.ts
@@ -1,7 +1,7 @@
-let x=5
-var bob = "timmy"
+let x = 5;
+var bob = "timmy";
 
-let list1 = [1,2,3, 5, 6]
-list1.forEach(function(entry) {
-    console.log(entry)
+let list1 = [1, 2, 3, 5, 6];
+list1.forEach(function (entry) {
+  console.log(entry);
 });
