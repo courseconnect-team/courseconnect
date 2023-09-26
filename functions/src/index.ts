76b3e8d408803693296c3cdd8ed7f318f04f2e07
MrinallU <mrinalluma@gmail.com>
2023-09-26 18:28:02 -0400

    Add inital signup form input validation

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -244,27 +244,3 @@ export const deleteUserFromID = functions.https.onRequest(
   }
 );
 
-export const checkIfIDInDatabase = functions.https.onRequest(
-  (request, response) => {
-
-
-    if (request.method === 'OPTIONS') {
-      // Handle preflight request
-      response.status(204).send('');
-    } else {
-      // Handle other requests
-      const userObject = {
-        school_id: request.body.ufid,
-      };
-
-      const usersRef = db.collection('users');
-      const snapshot = usersRef.where('ufid', '==', userObject.school_id).get();
-
-      if (!snapshot.empty) {
-        console.log('User with same school ID already exists!');
-        response.status(500).send('User with same school ID already exists!');
-      }
-      response.status(200).send('No user with that school ID found!');
-    }
-  }
-);
