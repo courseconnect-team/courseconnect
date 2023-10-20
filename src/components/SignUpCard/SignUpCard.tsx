2325d4cdacbafa80d7d795ef86994ee3a27d28ab
MrinallU <mrinalluma@gmail.com>
2023-10-20 06:24:12 -0400

    Sign up page

--- src/components/SignUpCard/SignUpCard.tsx
+++ src/components/SignUpCard/SignUpCard.tsx
@@ -2,19 +2,165 @@
 import React from "react";
 import { useState } from "react";
 import "./style.css";
-import { FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField } from "@mui/material";
+import { FormControl, FormControlLabel, FormLabel, InputLabel, MenuItem, Radio, RadioGroup, Select, SelectChangeEvent, Snackbar, TextField } from "@mui/material";
+import handleSignUp from '../../firebase/auth/auth_signup_password';
+import handleSignIn from '@/firebase/auth/auth_signin_password';
 
+import MuiAlert, { AlertProps } from '@mui/material/Alert';
+import { toast } from "react-hot-toast";
 export const SignUpCard = ({ className }: { className: any }) => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [password, setPassword] = useState("");
-  const [ufid, setUFID] = useState(-1);
-  const [department, setDepartment] = useState("");
+  const [ufid, setUFID] = useState("");
+  const [department, setDepartment] = useState('');
   const [role, setRole] = useState("");
   const [confirmedPassword, setConfirmedPassword] = useState("");
   const [email, setEmail] = useState("");
+  const [label, setLabel] = useState("Department");
+  const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+    props,
+    ref,
+  ) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
+  function isStrongPassword(password: string): boolean {
+    // Check if the password is at least 8 characters long
+    if (password.length < 8) {
+      toast.error('Password should contain at least 8 characters!');
+      return false;
+    }
+
+    const uppercaseRegex = /[A-Z]/;
+    const lowercaseRegex = /[a-z]/;
+    const numberRegex = /[0-9]/;
+    const specialCharacterRegex = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/;
+
+    if (!uppercaseRegex.test(password)) {
+      toast.error('Password should contain at least one uppercase letter!');
+      return false;
+    }
+    if (!lowercaseRegex.test(password)) {
+      toast.error('Password should contain at least one lowercase letter!')
+
+      return false;
+    }
+    if (!numberRegex.test(password)) {
+      toast.error('Password should contain at least one number!')
+
+      return false;
+    }
+    if (!specialCharacterRegex.test(password)) {
+      toast.error('Password should contain at least one special case character!')
+
+      return false;
+    }
+
+    return true;
+  }
+
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const handleSubmit = async (event: any) => {
+    setLoading(true);
+    event.preventDefault();
+    // extract the specific user data from the form data into a parsable object
+    const userData = {
+      firstname: firstName,
+      lastname: lastName,
+      email: email,
+      password: password,
+      confirmedpassword: confirmedPassword,
+      department: department,
+      role: role,
+      ufid: ufid,
+      uid: '',
+    };
+    console.log("Role " + userData.role);
+
+    // add the following:
+    if (userData.firstname === '') {
+      toast.error('Please enter a first name!');
+    } else if (/[0-9]/.test(userData.firstname)) {
+      toast.error('First name should only contain letters!');
+    } else if (userData.lastname == '') {
+      toast.error('Please enter a last name!');
+    } else if (userData.ufid == '') {
+      toast.error('Please enter your UFID!');
+    } else if (!/^[0-9]+$/.test(userData.ufid) || userData.ufid.length < 6) {
+      toast.error('UFID should only contain numbers with no spaces or dashes!');
+    } else if (userData.role === null || userData.role === "") {
+      toast.error('Please select a role!');
+    } else if (userData.department === '') {
+      toast.error('Please select a department!')
+    } else if (userData.password === '') {
+      toast.error('Please enter a password!');
+    } else if (userData.confirmedpassword != userData.password) {
+      toast.error('Passwords should match!');
+    } else if (!isStrongPassword(userData.password)) {
+      console.log("invalid password");
+    } else {
+      const uid_from_signup = await handleSignUp(
+        userData.firstname + ' ' + userData.lastname,
+        userData.email,
+        userData.password,
+        userData.ufid
+      );
+      userData.uid = uid_from_signup;
+      console.log(userData.uid);
+
+      if (userData.uid === '-1' || userData.uid === '') {
+        toast.error('This UFID is Already in Use!');
+
+        // error: user not created
+        // display some kind of snackbar or toast saying UFID is already in use
+      } else if (
+        userData.uid === '-2' ||
+        userData.uid === '-4' ||
+        userData.uid == '-3'
+      ) {
+        toast.error('Please Enter a Valid Email Adress!');
+      } else if (userData.uid == '-5') {
+        toast.error('Email adress is already in use by another account!');
+      } else {
+        // use fetch to send the user data to the server
+        // this goes to a cloud function which creates a document based on
+        // the data from the form, identified by the user's firebase auth uid
+
+        const response = await fetch(
+          'https://us-central1-courseconnect-c6a7b.cloudfunctions.net/processSignUpForm',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userData),
+          }
+        );
+
+        if (response.ok) {
+          setSuccess(true);
+          console.log('SUCCESS: User data sent to server successfully');
+          // then, sign in the user
+          handleSignIn(userData.email, userData.password);
+        } else {
+          console.log('ERROR: User data failed to send to server');
+          // display some kind of snackbar or toast saying data failed to send to server
+        }
+      }
+    }
+
+    setLoading(false);
+  };
+
+
   return (
     <div className="box">
+      <Snackbar open={success} autoHideDuration={3000}>
+        <Alert severity="info" sx={{ width: '100%' }}>
+          Signup successfull!
+        </Alert>
+      </Snackbar>
       <div className="log-in-card">
         <div className="overlap">
           <div className="welcome-to-course">
@@ -54,22 +200,44 @@ export const SignUpCard = ({ className }: { className: any }) => {
                 row
                 aria-labelledby="demo-row-radio-buttons-group-label"
                 name="row-radio-buttons-group"
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  setRole(event.target.value);
+                }}
+                value={role}
               >
-                <FormControlLabel value="female" control={<Radio />} label="Student" />
-                <FormControlLabel value="male" control={<Radio />} label="Staff" />
+                <FormControlLabel value="Student" control={<Radio />} label="Student" />
+                <FormControlLabel value="Faculty" control={<Radio />} label="Staff" />
               </RadioGroup>
             </FormControl>
           </div>
           <div className="department-input">
             <div className="department-dropdown">
-              <div className="text-wrapper-7">Department*</div>
+              <div className="text-wrapper-7">Department</div>
               <div className="overlap-group-2">
-                <div className="text-wrapper-8">Department</div>
-                <img
-                  className="arrow-drop-down"
-                  alt="Arrow drop down"
-                  src="https://c.animaapp.com/znqU6NK3/img/arrow-drop-down@2x.png"
-                />
+                <TextField
+                  sx={{ boxShadow: 'none', '.MuiOutlinedInput-notchedOutline': { border: 0 } }}
+                  value={department}
+                  placeholder="First Name"
+                  InputLabelProps={{ shrink: false }}
+                  label={label}
+                  select
+                  onChange={(event) => {
+                    setDepartment(event.target.value);
+                    setLabel("");
+                  }}
+                  className="text-wrapper-9"
+                >
+                  <MenuItem value={'ECE'}>ECE</MenuItem>
+                  <MenuItem value={'CISE'}>CISE</MenuItem>
+                  <MenuItem value={'ESSIE'}>ESSIE</MenuItem>
+                  <MenuItem value={'MAE'}>MAE</MenuItem>
+                  <MenuItem value={'MSE'}>MSE</MenuItem>
+                  <MenuItem value={'ABE'}>ABE</MenuItem>
+                  <MenuItem value={'CHE'}>CHE</MenuItem>
+                  <MenuItem value={'ISE'}>ISE</MenuItem>
+                  <MenuItem value={'EED'}>EED</MenuItem>
+                  <MenuItem value={'BME'}>BME</MenuItem>
+                </TextField>
               </div>
             </div>
           </div>
@@ -95,7 +263,7 @@ export const SignUpCard = ({ className }: { className: any }) => {
             </div>
           </div>
           <div className="UFID-input">
-            <div className="text-wrapper-9">UFID*</div>
+            <div className="text-wrapper-9">UFID</div>
             <div className="overlap-group-wrapper">
               <div className="overlap-group">
                 <TextField variant="standard"
@@ -108,7 +276,7 @@ export const SignUpCard = ({ className }: { className: any }) => {
                   type="number"
                   size="small"
                   onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                    setFirstName(event.target.value);
+                    setUFID(event.target.value);
                   }}
                   id="ufid"
                   name="ufid"
@@ -179,12 +347,12 @@ export const SignUpCard = ({ className }: { className: any }) => {
               </div>
             </div>
           </div>
-          <button className="sign-in-button">
+          <div className="sign-in-button">
             <br />
-            <div className="overlap-2">
+            <button onClick={(e) => handleSubmit(e)} className="overlap-2">
               <div className="text-wrapper-16">Sign up</div>
-            </div>
-          </button>
+            </button>
+          </div>
         </div>
 
       </div>
