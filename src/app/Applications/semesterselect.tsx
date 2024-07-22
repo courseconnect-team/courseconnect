678b79eb6baa2916ac56fc802b8a72f01650d257
MrinallU <mrinalluma@gmail.com>
2024-07-22 13:47:30 +0530

    modify semester select (get rid of fall 2022)

--- src/app/Applications/semesterselect.tsx
+++ src/app/Applications/semesterselect.tsx
@@ -13,14 +13,13 @@ const MenuProps = {
       maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
       width: 250,
       borderColor: '#000000', // Add border color
-      border: '1px solid #000000', 
-      borderRadius: "20px" 
+      border: '1px solid #000000',
+      borderRadius: "20px"
     },
   },
 };
 
 const names = [
-  'Fall 2022',
   'Fall 2024',
   'Spring 2025',
   'Summer 2025',
@@ -28,11 +27,11 @@ const names = [
 ];
 
 interface PageProps {
-  semester: string ;
+  semester: string;
   setSemester: (value: string) => void;
 }
 
-const SemesterSelect: React.FunctionComponent<PageProps> = ({semester, setSemester }) => {
+const SemesterSelect: React.FunctionComponent<PageProps> = ({ semester, setSemester }) => {
   const [personName, setPersonName] = React.useState<string>('');
 
   const handleChange = (event: SelectChangeEvent<string>) => {
@@ -41,13 +40,13 @@ const SemesterSelect: React.FunctionComponent<PageProps> = ({semester, setSemest
 
   return (
     <div>
-      <FormControl sx={{ m: 1, width: 336}}>
+      <FormControl sx={{ m: 1, width: 336 }}>
         <Select
           displayEmpty
           value={personName}
           onChange={handleChange}
-          
-          style={{borderRadius: "20px" , borderColor: "#000000"}}
+
+          style={{ borderRadius: "20px", borderColor: "#000000" }}
           sx={{
             "& .MuiOutlinedInput-notchedOutline": {
               borderColor: "#000000", // Change border color to black
@@ -55,34 +54,34 @@ const SemesterSelect: React.FunctionComponent<PageProps> = ({semester, setSemest
             "& .MuiSelect-select": {
               color: '#000', // Ensure text color remains black always
             },
-            
+
           }}
           renderValue={(selected) => {
             if (selected === '') {
-              return <em style = {{fontFamily: 'SF Pro Display'}}>Semester: <span style={{fontWeight: "bold", letterSpacing: "-0.75px"}}>{semester}</span></em>;
+              return <em style={{ fontFamily: 'SF Pro Display' }}>Semester: <span style={{ fontWeight: "bold", letterSpacing: "-0.75px" }}>{semester}</span></em>;
             }
             setSemester(selected);
-            return <em style = {{fontFamily: 'SF Pro Display'}}>Semester: <span style={{fontWeight: "bold", letterSpacing: "-0.75px"}}> {semester}</span></em>;
+            return <em style={{ fontFamily: 'SF Pro Display' }}>Semester: <span style={{ fontWeight: "bold", letterSpacing: "-0.75px" }}> {semester}</span></em>;
           }}
           MenuProps={MenuProps}
           inputProps={{ 'aria-label': 'Without label' }}
         >
           {names.map((name) => (
-             <MenuItem 
-             key={name} 
-             value={name}
-             sx={{
-               '&:hover': {
-                 fontWeight: 'bold', // Make text bold on hover
-                 
-               },
-              fontWeight: name === personName ? 'bold' : 'normal', 
-              borderRadius: "20px",
-              marginLeft: "11px",
-              marginRight: "11px"
-             }}
-           >
-              <em style={{fontFamily: 'SF Pro Display', letterSpacing: "-0.75px"}}>{name}</em>
+            <MenuItem
+              key={name}
+              value={name}
+              sx={{
+                '&:hover': {
+                  fontWeight: 'bold', // Make text bold on hover
+
+                },
+                fontWeight: name === personName ? 'bold' : 'normal',
+                borderRadius: "20px",
+                marginLeft: "11px",
+                marginRight: "11px"
+              }}
+            >
+              <em style={{ fontFamily: 'SF Pro Display', letterSpacing: "-0.75px" }}>{name}</em>
             </MenuItem>
           ))}
         </Select>
