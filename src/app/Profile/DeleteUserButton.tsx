3cb5323557444eee6c54898b168b8606e610d976
Raiden Williams <raidenwilliams@Raidens-MacBook-Pro.local>
2025-03-16 20:40:42 -0400

    updating sizing and styling of delete user flow

--- src/app/Profile/DeleteUserButton.tsx
+++ src/app/Profile/DeleteUserButton.tsx
@@ -1,12 +1,9 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import PersonRemoveIcon from '@mui/icons-material/PersonRemove';
 import PersonRemoveOutlinedIcon from '@mui/icons-material/PersonRemoveOutlined';
 import { HandleDeleteUser } from '@/firebase/auth/auth_delete_prompt';
 
@@ -50,25 +47,98 @@ const DeleteUserDialog: React.FC<CreateCourseDialogProps> = ({
         variant="contained"
         startIcon={<PersonRemoveOutlinedIcon />}
         onClick={handleClickOpen}
-        style={{ backgroundColor: '#5736ac', color: '#ffffff', borderRadius: "10px", height: "53px", width: "180px", textTransform: "none" }}
+        style={{
+          backgroundColor: '#5736ac',
+          color: '#ffffff',
+          borderRadius: '8px',
+          height: '50px',
+          width: '200px',
+          textTransform: 'none',
+        }}
       >
         Delete User
       </Button>
 
-      <Dialog style={{ borderImage: "linear-gradient(to bottom, rgb(9, 251, 211), rgb(255, 111, 241)) 1", boxShadow: "0px 2px 20px 4px #00000040", borderRadius: "20px", border: "2px solid" }} PaperProps={{
-        style: { borderRadius: 20 }
-      }} open={open} onClose={handleClose} >
-        <DialogTitle style={{ fontFamily: "SF Pro Display-Medium, Helvetica", textAlign: "center", fontSize: "35px", fontWeight: "540" }}>Delete Applicant</DialogTitle>
+      <Dialog
+        style={{
+          borderImage:
+            'linear-gradient(to bottom, rgb(9, 251, 211), rgb(255, 111, 241)) 1',
+          boxShadow: '0px 2px 20px 4px #00000040',
+          border: '2px solid',
+        }}
+        PaperProps={{
+          style: { borderRadius: '16px' },
+        }}
+        open={open}
+        onClose={handleClose}
+      >
+        <DialogTitle
+          style={{
+            fontFamily: 'SF Pro Display-Medium, Helvetica',
+            textAlign: 'center',
+            fontSize: '30px',
+            padding: '4px',
+            fontWeight: '400',
+          }}
+        >
+          Delete Applicant
+        </DialogTitle>
         <form onSubmit={handleSubmit}>
-          <DialogContent>
-            <DialogContentText style={{ marginTop: "35px", fontFamily: "SF Pro Display-Medium, Helvetica", textAlign: "center", fontSize: "24px", color: "black" }}>
-              Are you sure you want to delete this user?
-            </DialogContentText>
+          <DialogContent
+            style={{
+              fontFamily: 'SF Pro Display-Medium, Helvetica',
+              textAlign: 'center',
+              fontSize: '20px',
+              color: 'black',
+              padding: '8px',
+              marginLeft: '16px',
+              marginRight: '16px',
+            }}
+          >
+            Are you sure you want to delete this user?
           </DialogContent>
-          <DialogActions style={{ marginTop: "30px", marginBottom: "42px", display: "flex", justifyContent: "space-between", gap: "93px" }}>
-            <Button variant="outlined" style={{ fontSize: "17px", marginLeft: "110px", borderRadius: "10px", height: '43px', width: '120px', textTransform: "none", fontFamily: "SF Pro Display-Bold , Helvetica", borderColor: '#5736ac', color: '#5736ac', borderWidth: "3px" }} onClick={handleClose}>Cancel</Button>
+          <DialogActions
+            style={{
+              padding: '16px',
+              display: 'flex',
+              justifyContent: 'space-evenly',
+            }}
+          >
+            <Button
+              variant="outlined"
+              style={{
+                fontSize: '16px',
+                borderRadius: '8px',
+                height: '40px',
+                width: '120px',
+                textTransform: 'none',
+                fontFamily: 'SF Pro Display-Bold , Helvetica',
+                borderColor: '#5736ac',
+                color: '#5736ac',
+                borderWidth: '2px',
+              }}
+              onClick={handleClose}
+            >
+              Cancel
+            </Button>
 
-            <Button variant="contained" style={{ fontSize: "17px", marginRight: "110px", borderRadius: "10px", height: '43px', width: '120px', textTransform: "none", fontFamily: "SF Pro Display-Bold , Helvetica", backgroundColor: '#5736ac', color: '#ffffff' }} type="submit">Delete</Button>
+            <Button
+              variant="contained"
+              style={{
+                fontSize: '16px',
+                marginLeft: '0px',
+                borderRadius: '8px',
+                height: '40px',
+                width: '120px',
+                textTransform: 'none',
+                fontFamily: 'SF Pro Display-Bold , Helvetica',
+                backgroundColor: '#5736ac',
+                color: '#ffffff',
+              }}
+              type="submit"
+            >
+              Delete
+            </Button>
           </DialogActions>
         </form>
       </Dialog>
