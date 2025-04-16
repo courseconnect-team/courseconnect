a994b4e13b9e224ad3902e4a7e03b724c8f84338
John Glass <50562358+johnglass2003@users.noreply.github.com>
2025-04-16 14:51:42 -0400

    Better Create New Position Button Form

--- src/components/Research/Modal.tsx
+++ src/components/Research/Modal.tsx
@@ -7,11 +7,17 @@ import {
   Button,
   TextField,
   SxProps,
+  Grid,
+  MenuItem,
+  Checkbox,
+  InputLabel,
+  FormControlLabel,
 } from '@mui/material';
 import firebase from '@/firebase/firebase_config';
 import { collection, addDoc } from 'firebase/firestore';
 import { Theme } from '@emotion/react';
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 
 /** Define an interface that matches your JSON keys (without using any values). */
 interface FormData {
@@ -38,7 +44,7 @@ interface ResearchModal {
   buttonStyle?: SxProps<Theme>;
   buttonText: string;
   firebaseQuery: (formData: any) => Promise<void>;
-  uid: string
+  uid: string;
 }
 const ResearchModal: React.FC<ResearchModal> = ({
   onSubmitSuccess,
@@ -46,10 +52,12 @@ const ResearchModal: React.FC<ResearchModal> = ({
   buttonStyle,
   buttonText,
   firebaseQuery,
-  uid
+  uid,
 }) => {
   const [open, setOpen] = useState(false);
   const [formData, setFormData] = useState<FormData>(currentFormData);
+  const [imageFile, setImageFile] = useState<File | null>(null);
+  const [imageUploading, setImageUploading] = useState(false);
 
   /** Opens the dialog (modal). */
   const handleOpen = () => {
@@ -72,24 +80,54 @@ const ResearchModal: React.FC<ResearchModal> = ({
     setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
-  /** Submits the form and clears it, then closes the dialog. */
-  async function handleSubmit() {
-    // Do something with the form data, e.g., console.log or send to a server
-
-    // Reset form data only on submit
-    const finalFormData = {
-        ...formData,
-        id: uuidv4(),
-        creator_id: uid, 
-        faculty_members: [uid],
-        applications: [],
+  /** Handles image file selection. */
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      setImageFile(event.target.files[0]);
     }
+  };
+
+  /** Uploads the image to Firebase Storage and returns the download URL. */
+  const uploadImage = async (): Promise<string | null> => {
+    if (!imageFile) return null;
+
+    const storage = getStorage();
+    const storageRef = ref(
+      storage,
+      `research-images/${uuidv4()}-${imageFile.name}`
+    );
+    setImageUploading(true);
+
+    try {
+      await uploadBytes(storageRef, imageFile);
+      const downloadURL = await getDownloadURL(storageRef);
+      return downloadURL;
+    } catch (error) {
+      console.error('Error uploading image:', error);
+      return null;
+    } finally {
+      setImageUploading(false);
+    }
+  };
+
+  /** Submits the form and clears it, then closes the dialog. */
+  const handleSubmit = async () => {
+    const imageUrl = await uploadImage();
+
+    const finalFormData = {
+      ...formData,
+      id: uuidv4(),
+      creator_id: uid,
+      faculty_members: [uid],
+      applications: [],
+      image_url: imageUrl || '', // Add the image URL to the form data
+    };
+
     firebaseQuery(finalFormData);
-    // Close the modal
     onSubmitSuccess();
     setFormData(currentFormData);
     handleClose();
-  }
+  };
 
   return (
     <div>
@@ -101,117 +139,272 @@ const ResearchModal: React.FC<ResearchModal> = ({
       <Dialog open={open} onClose={handleClose} fullWidth maxWidth="md">
         <DialogTitle>{buttonText}</DialogTitle>
         <DialogContent>
-          {/* Render each field as a TextField, using the JSON keys as "name" */}
-          <TextField
-            margin="dense"
-            label="Project Title"
-            name="project_title"
-            value={formData.project_title}
-            onChange={handleChange}
-            fullWidth
-          />
-          <TextField
-            margin="dense"
-            label="Department"
-            name="department"
-            value={formData.department}
-            onChange={handleChange}
-            fullWidth
-          />
-          <TextField
-            margin="dense"
-            label="Faculty Mentor"
-            name="faculty_mentor"
-            value={formData.faculty_mentor}
-            onChange={handleChange}
-            fullWidth
-          />
-          <TextField
-            margin="dense"
-            label="PhD Student Mentor"
-            name="phd_student_mentor"
-            value={formData.phd_student_mentor}
-            onChange={handleChange}
-            fullWidth
-          />
-          <TextField
-            margin="dense"
-            label="Terms Available"
-            name="terms_available"
-            value={formData.terms_available}
-            onChange={handleChange}
-            fullWidth
-          />
-          <TextField
-            margin="dense"
-            label="Student Level"
-            name="student_level"
-            value={formData.student_level}
-            onChange={handleChange}
-            fullWidth
-          />
-          <TextField
-            margin="dense"
-            label="Prerequisites"
-            name="prerequisites"
-            value={formData.prerequisites}
-            onChange={handleChange}
-            fullWidth
-          />
-          <TextField
-            margin="dense"
-            label="Credit"
-            name="credit"
-            value={formData.credit}
-            onChange={handleChange}
-            fullWidth
-          />
-          <TextField
-            margin="dense"
-            label="Stipend"
-            name="stipend"
-            value={formData.stipend}
-            onChange={handleChange}
-            fullWidth
-          />
-          <TextField
-            margin="dense"
-            label="Application Requirements"
-            name="application_requirements"
-            value={formData.application_requirements}
-            onChange={handleChange}
-            fullWidth
-          />
-          <TextField
-            margin="dense"
-            label="Application Deadline"
-            name="application_deadline"
-            value={formData.application_deadline}
-            onChange={handleChange}
-            fullWidth
-          />
-          <TextField
-            margin="dense"
-            label="Website"
-            name="website"
-            value={formData.website}
-            onChange={handleChange}
-            fullWidth
-          />
-          <TextField
-            margin="dense"
-            label="Project Description"
-            name="project_description"
-            value={formData.project_description}
-            onChange={handleChange}
-            fullWidth
-            multiline
-            rows={4}
-          />
+          <Grid container spacing={2}>
+            {/* Project Title */}
+            <Grid item xs={12}>
+              <TextField
+                margin="dense"
+                label="Project Title"
+                name="project_title"
+                value={formData.project_title}
+                onChange={handleChange}
+                fullWidth
+              />
+            </Grid>
+
+            {/* Department */}
+            <Grid item xs={12} sm={6}>
+              <TextField
+                margin="dense"
+                label="Department"
+                name="department"
+                value={formData.department}
+                onChange={handleChange}
+                fullWidth
+              />
+            </Grid>
+
+            {/* Faculty Mentor */}
+            <Grid item xs={12} sm={6}>
+              <TextField
+                margin="dense"
+                label="Faculty Mentor"
+                name="faculty_mentor"
+                value={formData.faculty_mentor}
+                onChange={handleChange}
+                fullWidth
+              />
+            </Grid>
+
+            {/* PhD Student Mentor */}
+            <Grid item xs={12} sm={6}>
+              <TextField
+                margin="dense"
+                label="PhD Student Mentor"
+                name="phd_student_mentor"
+                value={formData.phd_student_mentor}
+                onChange={handleChange}
+                fullWidth
+              />
+            </Grid>
+
+            {/* Terms Available */}
+            <Grid item xs={12} sm={6}>
+              <TextField
+                margin="dense"
+                label="Terms Available"
+                name="terms_available"
+                value={formData.terms_available}
+                onChange={handleChange}
+                fullWidth
+              />
+            </Grid>
+
+            {/* Student Level */}
+            <Grid item xs={12}>
+              <TextField
+                select
+                label="Student Level"
+                name="student_level"
+                value={formData.student_level}
+                onChange={(e) =>
+                  setFormData({ ...formData, student_level: e.target.value })
+                }
+                fullWidth
+                SelectProps={{
+                  renderValue: (selected) => selected.split(',').join(', '),
+                }}
+              >
+                {['Freshman', 'Sophomore', 'Junior', 'Senior'].map((level) => (
+                  <MenuItem key={level} value={level.toLowerCase()}>
+                    <Checkbox
+                      checked={formData.student_level.includes(
+                        level.toLowerCase()
+                      )}
+                      onChange={(e) => {
+                        const { value, checked } = e.target;
+                        setFormData((prev) => ({
+                          ...prev,
+                          student_level: checked
+                            ? [...prev.student_level.split(','), value].join(
+                                ','
+                              )
+                            : prev.student_level
+                                .split(',')
+                                .filter((item) => item !== value)
+                                .join(','),
+                        }));
+                      }}
+                    />
+                    {level}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </Grid>
+
+            {/* Prerequisites */}
+            <Grid item xs={12} sm={6}>
+              <TextField
+                margin="dense"
+                label="Prerequisites"
+                name="prerequisites"
+                value={formData.prerequisites}
+                onChange={handleChange}
+                fullWidth
+              />
+            </Grid>
+
+            {/* Credit */}
+            <Grid item xs={12} sm={6}>
+              <TextField
+                margin="dense"
+                label="Credit"
+                name="credit"
+                type="number"
+                inputProps={{ min: 0, max: 9 }}
+                value={formData.credit}
+                onChange={handleChange}
+                fullWidth
+              />
+            </Grid>
+
+            {/* Stipend */}
+            <Grid item xs={12} sm={6}>
+              <TextField
+                margin="dense"
+                label="Stipend"
+                name="stipend"
+                value={formData.stipend}
+                onChange={handleChange}
+                fullWidth
+              />
+            </Grid>
+
+            {/* Website */}
+            <Grid item xs={12} sm={6}>
+              <TextField
+                margin="dense"
+                label="Website"
+                name="website"
+                value={formData.website}
+                onChange={handleChange}
+                fullWidth
+              />
+            </Grid>
+
+            {/* Application Requirements */}
+            <Grid item xs={12}>
+              <TextField
+                margin="dense"
+                label="Application Requirements"
+                name="application_requirements"
+                value={formData.application_requirements}
+                onChange={handleChange}
+                fullWidth
+              />
+            </Grid>
+
+            {/* Application Deadline */}
+            <Grid item xs={12} sm={6}>
+              <InputLabel shrink>Application Deadline</InputLabel>
+              <TextField
+                name="application_deadline"
+                type="date"
+                value={
+                  formData.application_deadline === 'rolling'
+                    ? ''
+                    : formData.application_deadline
+                }
+                onChange={handleChange}
+                fullWidth
+                disabled={formData.application_deadline === 'rolling'}
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={formData.application_deadline === 'rolling'}
+                    onChange={(e) => {
+                      setFormData((prev) => ({
+                        ...prev,
+                        application_deadline: e.target.checked ? 'rolling' : '',
+                      }));
+                    }}
+                  />
+                }
+                label="Rolling"
+              />
+            </Grid>
+
+            {/* Project Description */}
+            <Grid item xs={12}>
+              <TextField
+                margin="dense"
+                label="Project Description"
+                name="project_description"
+                value={formData.project_description}
+                onChange={handleChange}
+                fullWidth
+                multiline
+                rows={4}
+              />
+            </Grid>
+
+            {/* Image Upload Field */}
+            <Grid item xs={12}>
+              <Button
+                component="label"
+                sx={{
+                  backgroundColor: '#5A41D8',
+                  color: '#FFFFFF',
+                  textTransform: 'none',
+                  borderRadius: '8px',
+                  boxShadow: '0px 0px 8px #E5F0DC',
+                  fontWeight: 500,
+                  padding: '10px 24px',
+                  marginTop: '16px',
+                  '&:hover': {
+                    backgroundColor: '#5A41D8',
+                    boxShadow: '0px 0px 8px #E5F0DC',
+                  },
+                }}
+              >
+                Upload Image
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={handleImageChange}
+                  hidden
+                />
+              </Button>
+              {imageUploading && <p>Uploading image...</p>}
+            </Grid>
+          </Grid>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose}>Close</Button>
-          <Button variant="contained" onClick={handleSubmit}>
+          <Button
+            onClick={handleClose}
+            sx={{
+              textTransform: 'none',
+              color: '#5A41D8',
+              fontWeight: 500,
+            }}
+          >
+            Close
+          </Button>
+          <Button
+            variant="contained"
+            onClick={handleSubmit}
+            disabled={imageUploading}
+            sx={{
+              backgroundColor: '#5A41D8',
+              color: '#FFFFFF',
+              textTransform: 'none',
+              fontWeight: 500,
+              '&:hover': {
+                backgroundColor: '#5A41D8',
+              },
+            }}
+          >
             Submit
           </Button>
         </DialogActions>
