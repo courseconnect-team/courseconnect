7dfeb75c026233d7eae1cae53389768fd736cbaf
RelativelyBlank <jeffreyma1008@gmail.com>
2025-04-21 17:10:21 -0400

    students can only apply once

--- src/components/Research/StudentResearchView.tsx
+++ src/components/Research/StudentResearchView.tsx
@@ -167,6 +167,8 @@ const StudentResearchView: React.FC<StudentResearchViewProps> = ({
               {researchListings.map((item, index) => (
                 <Grid item xs={12} sm={6} md={6} key={index}>
                   <ProjectCard
+                    uid={uid}
+                    applications={item.applications}
                     listingId={item.id}
                     userRole={role}
                     project_title={item.project_title}
